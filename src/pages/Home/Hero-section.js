8954c80865fa1c7d01eca553f9e6b53a1fe9a7d1
gagan goswami <143880202+gagan-goswami@users.noreply.github.com>
2024-08-29 00:17:48 +0530

    Update Hero-section.js

--- src/pages/Home/Hero-section.js
+++ src/pages/Home/Hero-section.js
@@ -20,8 +20,8 @@ function Hero() {
     dots: true,
     nav: true,
     navText: [
-      '<span class="custom-prev-icon">🡸</span>', // Custom previous icon
-      '<span class="custom-next-icon">🡺</span>', // Custom next icon
+      '<span class="custom-prev-icon">⬅</span>', // Custom previous icon
+      '<span class="custom-next-icon">➡</span>', // Custom next icon
     ],
     navClass: ["owl-prev", "owl-next"],
     responsive: {
